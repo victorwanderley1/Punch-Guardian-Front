2d52778e77bebd16a9ac6b7f2d8ffd886d82ec87
Victor Wanderley <victorwasouza@gmail.com>
2023-12-02 17:45:59 -0300

    feat: Adicionando relógio e input para período

--- src/app/registro-pontos/registro-pontos.component.ts
+++ src/app/registro-pontos/registro-pontos.component.ts
@@ -1,22 +1,33 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 import { RegistroPontosApiService } from "../registro-pontos-api.service";
 import { RegistroPonto } from './registro-ponto.interface';
 import { EspelhoPonto } from './espelho-ponto/espelho-ponto.interface';
+import { MatDateFormats, MAT_DATE_FORMATS } from '@angular/material/core';
+
 
 @Component({
   selector: 'app-registro-pontos',
   templateUrl: './registro-pontos.component.html',
-  styleUrls: ['./registro-pontos.component.css']
+  styleUrls: ['./registro-pontos.component.css'],
+  providers: [
+  ]
 })
 export class RegistroPontosComponent {
+  range = new FormGroup({
+    dtInicio: new FormControl<Date | null>(null),
+    dtFim: new FormControl<Date | null>(null),
+  });
   pontos: RegistroPonto[] = [];
   pontoPrincipal: any = {};
   mensagem: string = '';
   espelhoPonto!: EspelhoPonto;
   columnsToDisplay = ['data', 'entrada1', 'saida1', 'entrada2', 'saida2', 'totalHoras'];
+  timeString: string = '00:00:00';
   constructor(private registroService: RegistroPontosApiService){ }
   ngOnInit(){
-    
+    this.updateTime(); // Chama a função para exibir o horário inicialmente
+    setInterval(() => this.updateTime(), 1000); // Atualiza a cada segundo
   }
 
   buscarPontos(){
@@ -38,7 +49,7 @@ export class RegistroPontosComponent {
     this.registroService.baterPonto().subscribe((mensagem: any)=>{
       console.log(mensagem);
       this.mensagem = mensagem.mensagemRetorno;
-      this.buscaEspelhoPonto();
+      // this.buscaEspelhoPonto();
     });
   }
 
@@ -46,4 +57,18 @@ export class RegistroPontosComponent {
     return Object.keys(pontos).sort();
   }
 
-}
+  updateTime() {
+    const now = new Date();
+    const hours = this.padNumber(now.getHours());
+    const minutes = this.padNumber(now.getMinutes());
+    const seconds = this.padNumber(now.getSeconds());
+    this.timeString = `${hours}:${minutes}:${seconds}`;
+  }
+
+  private padNumber(num: number): string {
+    return num.toString().padStart(2, '0');
+  }
+  
+};
+
+
